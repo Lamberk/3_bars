b139bcac70b3ce62b31c9a9df9b9cb2fe1020ff4
Komarov Valentin <komarovvv36@gmail.com>
2016-09-24 11:25:00 +0300

    Delete comments

--- bars.py
+++ bars.py
@@ -19,13 +19,12 @@ def get_smallest_bar(data):
 
 def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
+    angle = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    radian_length = 2 * asin(sqrt(angle))
+    radius = 6371
+    return radian_length * radius
 
 
 def get_closest_bar(data, longitude, latitude):
